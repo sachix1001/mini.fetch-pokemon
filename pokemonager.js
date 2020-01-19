34c2e48411c66e21cbfae08bc891a572a2651847
Sachi Grannan <sachix1001@gmail.com>
2020-01-20 08:50:41 +0900

    basic requirement

--- pokemonager.js
+++ pokemonager.js
@@ -13,62 +13,33 @@
 
     // This should return an array of all the Pokemon that are under a particular weight.
 
-    findUnderWeight(weight) {
-      async function getpokemonList() {
-        const list = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10"
-        );
-        const arr = [];
-        const pokemonInfo = await list.json();
-        const pokemonList = await pokemonInfo.results;
-        pokemonList.map((pokemon) => {
-          async function getWeight() {
-            const url = await pokemon.url;
-            const pokemonJson = await fetch(url);
-            arr.push(await pokemonJson.json());
-            console.log(arr);
-            // const weight = await pokemonWeight.weight;
+    async findUnderWeight(weight) {
+      let weightList = [];
 
-            // val = weight;
-          }
-          getWeight();
-          return val;
-        });
+      for (let i = 1; i <= 10; i++) {
+        let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+        pokemon = await pokemon.json();
+        weightList.push(pokemon);
       }
-      return getpokemonList();
+      weightList = weightList.filter((pokemon) => pokemon.weight < weight);
+      return weightList;
     }
-  }
-  // Your code here.
-  // ** LIMIT TO THE FIRST 10 POKEMON
-  // We don't want to make too many unnecessary calls to the Pokemon API
-  // const pokemonWeight = [];
-  // fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=10")
+  // }
+  // findUnderWeight(weight) {
+  //   const arr = []
+  //   return fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=10")
   //   .then((pokemonP) => pokemonP.json())
-  //   .then((pokemonList) => {
-  //     // const pokemonWeight = [];
-
-  //     const temp = pokemonList.results.map((pokemon) => {
+  //   .then((pokemonList) => 
+  //      pokemonList.results.map((pokemon) => {
   //       const url = pokemon.url;
-  //       fetch(url)
-  //         .then((detailRequest) => detailRequest.json())
-  //         .then((detail) => {
-  //           // console.log(detail.weight);
-  //           pokemonWeight.push(detail.weight);
-  //           // const weight = detail.weight;
-  //         });
-  //       // console.log(weight);
-  //     });
-  //     Promise.all(temp);
-  //     // console.log(`pokemonList : ${pokemonList}`);
-  //     // console.log(`pokemonList.result : ${pokemonList.result}`);
-  //     // console.log(`X = ${x}`);
-  //     console.log(temp);
-  //     // return pokemonWeight;
-  //   });
-  // // .then(() => {
-  // //   console.log(pokemonWeight);
-  // // });
-  // // console.log(pokemonWeight);
-  // return pokemonWeight;
+  //       return fetch(url)
+  //       .then((detailRequest) => detailRequest.json())
+  //     })
+  //   )
+  //   .then(list => list.map(p => p.then(poke => {
+  //     arr.push(poke)
+  //   })))
+  // }
+}
   window.Pokemonager = Pokemonager;
 })();
